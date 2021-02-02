969b4e7a21cde4452967b4a5a8b45ab34ad25f72
SergioVargas13 <SergioVargas13@users.noreply.github.com>
2021-02-02 11:19:56 -0500

    plantilla

--- database/seeds/UserTablaSeeder.php
+++ database/seeds/UserTablaSeeder.php
@@ -20,19 +20,21 @@ class UserTablaSeeder extends Seeder
         $user->email='email3@gmail.com';
         $user->password=Hash::make('12345678');
         $user->save();
+        $user->roles()->attach(Role::where('name', 'Super Administrador')->get()->first());
 
         $user = new User();
         $user->name='Henry';
         $user->email='email2@gmail.com';
         $user->password=Hash::make('12345678');
         $user->save();
-
+        $user->roles()->attach(Role::where('name', 'Panadero')->get()->first());
+        
         $user = new User();
         $user->name='Yohana';
         $user->email='email1@gmail.com';
         $user->password=Hash::make('12345678');
         $user->save();
+        $user->roles()->attach(Role::where('name', 'Auxiliar')->get()->first());
 
-        $user->roles()->attach(Role::where('name', 'role')->get()->first());
     }
 }
